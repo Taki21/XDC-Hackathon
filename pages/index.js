69a43596a3c25c40e3207b54003c5ead90f86f80
Professor Taki <shakman21@gmail.com>
2023-01-08 20:35:38 -0500

    a

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>XDC Flashloans</title>
+        <title>Polygon Flashloans</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
